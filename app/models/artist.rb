4b92b94b336411287a1d66f1cb6a3dbce6ef502b
ELEXILON <elexilon@gmail.com>
2017-12-23 15:22:50 +0100

    Create Photo: Model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ApplicationRecord
   has_many :songs
-  
+  has_one :photo
+
   validates :name,        presence: true, length: { maximum: 255 }
   validates :description, presence: true, length: { maximum: 500 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
+Photo.destroy_all
+Song.destroy_all
 Artist.destroy_all
 
 
 20.times do
   artist = Artist.create!(name:   Faker::FunnyName.name,
                    description:   Faker::Hipster.paragraph[0..496] << "...")
+
+  Photo.create!(remote_image_url: "http://res.cloudinary.com/elexilon/image/upload/v1513334848/PROD-Waving-during-the-concert_isoiyb.jpg", artist: artist)
 
   10.times do
             Song.create!(name:    Faker::FunnyName.name,
